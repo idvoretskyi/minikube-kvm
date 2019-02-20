8ca3c5f9fcffa47064791ba0d9f449d94b60f3d1
Ihor Dvoretskyi <ihor@linux.com>
2019-02-20 16:18:56 +0000

    minikube-kvm-install.sh WIP
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

--- minikube-kvm-install.sh
+++ minikube-kvm-install.sh
@@ -1,78 +1,80 @@
+#!/bin/bash
+
 ### QEMU/KVM and libvirt installation
 
-_NOTE: skip if already installed_
+# _NOTE: skip if already installed_
 
-```shell
+
 sudo apt install libvirt-clients libvirt-daemon-system qemu-kvm \
     && sudo usermod -a -G libvirt $(whoami) \
     && newgrp libvirt
-```
+
 
 ### kubectl (Kubernetes CLI) installation
 
-_NOTE: skip if already installed_
+# _NOTE: skip if already installed_
 
-```shell
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
     && sudo install kubectl /usr/local/bin
-```
+
 
 ## Installation
 
 ### Minikube KVM driver installation
 
-```shell
+
 curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 \
     && sudo install docker-machine-driver-kvm2 /usr/local/bin/
-```
+
 
 ### Minikube installation
 
-```shell
+
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
     && sudo install minikube-linux-amd64 /usr/local/bin/minikube
-```
+
 
 ### To use the KVM2 driver:
 
-```shell
+
 minikube start --vm-driver kvm2
-```
+
 
 ### Set KVM2 as a default VM driver for Minikube
 
-```shell
+
 minikube config set vm-driver kvm2
-```
 
-and run minikube as usual:
 
-```shell
+# and run minikube as usual:
+
+
 minikube start
-```
+
 
 ## Verify the installation
 
-Verify if Minikube was installed properly:
+# Verify if Minikube was installed properly:
 
-```shell
+
 minikube version
-```
 
-Check out if the Kubernetes cluster is up and running:
 
-```shell
+# Check out if the Kubernetes cluster is up and running:
+
+
 kubectl get nodes
-```
 
-Run a sample simple app (nginx in our case)
 
-```shell
+# Run a sample simple app (nginx in our case)
+
+
 kubectl run nginx --image=nginx
-```
 
-And check out if the Kubernetes pods are properly provisioned
 
-```shell
+# And check out if the Kubernetes pods are properly provisioned
+
+
 kubectl get pods
-```
+
